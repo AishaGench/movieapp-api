95b08aa71bcdf3ac195d3f4c895bf87756758ad6
Aisha Gench <aisha.gench@gmail.com>
2021-06-23 20:16:21 -0700

    create models

---
+++ models/Movie.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const MovieSchema = new Schema({
+    director_id:Schema.Type.ObjectId,
+  title:{
+    type:String,
+    required:[true, 'The field `{PATH}` is required'],
+    maxlength:[50,'The field `{PATH}` must be less than {MAXLENGTH} characters']
+  },
+  category:{type:String, maxlength:30, minlength:1},
+  year:{type:Number, min:1850, max:2100},
+  imdb_score:{type:Number, max:10, min:0},
+  createdAt:{type:Date, default:Date.now}
+
+})
+
+
+
+module.exports = mongoose.model("movie", MovieSchema)
