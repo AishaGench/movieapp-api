2d1c7e5e2146a65ef96ccf07c603e1b10439ebb8
Aisha Gench <aisha.gench@gmail.com>
2021-06-29 21:56:49 -0500

    use aggregate

--- routes/director.routes.js
+++ routes/director.routes.js
@@ -4,9 +4,22 @@ const router = express.Router()
 // Model
 const DirectorModel = require('../models/Director') 
 
-// GET all directors
+// GET all directors with their movies
 router.get('/', (req, res,next)=>{
-  res.end('GET request to Director Page...')
+ /* //res.end('GET request to Director Page...')
+  //DirectorModel.find()*/
+  DirectorModel.aggregate([
+    {
+      $lookup:{
+        from:'movies',
+        localField:'_id',
+        foreignField:'director_id',
+        as:'movies'
+      }
+    }
+  ])
+  .then((directorList)=>{res.json(directorList)})
+  .catch((err)=>{next({message:err})})
 })
 
 router.post('/',(req, res, next)=>{
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,62 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
-// Import the model
-const MovieModel = require('../models/Movie')
+// Model
+const DirectorModel = require('../models/Director') 
 
-//Get request with node-express-get snippet
-router.get('/', (req, res) => {
-  MovieModel.find()
-  .then((movieList)=>res.json(movieList))
-  .catch((err)=>{res.json(err)})
-})
-//SORT => 1, ASC || -1, DESC
-
-//GET top 10 movies
-router.get('/top10',(req,res)=>{
-  MovieModel.find().sort({imdb_score:1}).limit(10)
-  .then((movieList)=>res.json(movieList))
-  .catch((err)=>{res.json(err)})
+// GET all directors
+router.get('/', (req, res,next)=>{
+  //res.end('GET request to Director Page...')
+  DirectorModel.find()
+  .then((directorList)=>{res.json(directorList)})
+  .catch((err)=>{next({message:err})})
 })
 
-//GET details of a movie /ap/movies/:movieId
-router.get('/:movieId',(req,res)=>{
-  MovieModel.findById(req.params.movieId)
-  .then((movie)=>{res.json(movie)})
-  .catch((err)=>{res.json(err)})
+router.post('/',(req, res, next)=>{
+  const newDirector = new DirectorModel(req.body)
+  newDirector.save()
+  .then((director)=>{res.json(director)})
+  .catch((err)=>{next({message:err})})
 })
-
-//List movies between specific date
-router.get('/between/:startYear/:endYear',(req,res)=>{
-  const {startYear,endYear}= req.params
-  MovieModel.find({year:{"$gte":parseInt(startYear), "$lte":parseInt(endYear)}})
-  .then((movieList)=>{res.json(movieList)})
-  .catch((err)=>{res.json(err)})
-})
-
-// POST request to save Movies in the DB
-router.post('/',(req, res,next)=>{
-  const newMovie = new MovieModel(req.body)
-  newMovie.save()
-  .then((movie)=>{res.json(movie)})
-  .catch((error)=>{next({message:error})
-                  /*res.json(error)*/})
-})
-
-//PUT Method to update a movie /api/movies/:movieId
-router.put('/:movieId',(req,res,next)=>{
-  MovieModel.findByIdAndUpdate(req.params.movieId, req.body,{new:true})
-  .then((movie)=>{res.json(movie)})
-  .catch((err)=>{res.json(err)})
-})
-
-//DELETE request to remove a movie from DB /api/movies/:movieId
-router.delete('/:movieId',(req,res,next)=>{
-  MovieModel.findByIdAndRemove(req.params.movieId)
-  .then((movie)=>{res.json(movie)})
-  .catch((err)=>{res.json(err)})
-})
-
-
-// Export the route
 module.exports = router
