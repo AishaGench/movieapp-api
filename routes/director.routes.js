1906f6e3e6340a45e402bd6e6347b12c44bb1b6f
Aisha Gench <aisha.gench@gmail.com>
2021-06-29 20:50:26 -0500

    use GET request all directors

--- routes/director.routes.js
+++ routes/director.routes.js
@@ -4,3 +4,10 @@ const router = express.Router()
 
 //Model
 const DirectorModel = require('../models/Director')
+
+//GET all directors
+router.get('/',(req, res, next)=>{
+    res.end('Get request to Director Page...')
+})
+
+module.exports =router
