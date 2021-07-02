517bec15d1da41ead45ba03f10c05abc60d9d41a
Aisha Gench <aisha.gench@gmail.com>
2021-07-01 21:56:14 -0500

    create a new user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+//Model
+const UserModel =require('../models/User')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('GET request from USERS');
 });
+
+//Create a User users/register
+router.post('/register',(req,res,next)=>{
+  const {username, password} = req.body
+  const newUser = new UserModel({username, password})
+  newUser.save()
+  .then(data =>res.json(data))
+  .catch(err =>{next({message:err})})
+})
 
 module.exports = router;
