8cb46a5fa534be3e29b1edd6f589eda0e5f5b407
Aisha Gench <aisha.gench@gmail.com>
2021-07-01 20:38:00 -0500

    get a director api/directors/:directorId

--- routes/director.routes.js
+++ routes/director.routes.js
@@ -6,8 +6,8 @@ const DirectorModel = require('../models/Director')
 
 // GET all directors with their movies
 router.get('/', (req, res,next)=>{
-  /*//res.end('GET request to Director Page...')
-  //DirectorModel.find()*/
+  //res.end('GET request to Director Page...')
+  //DirectorModel.find()
   DirectorModel.aggregate([
     {
       $lookup:{
@@ -31,6 +31,19 @@ router.get('/', (req, res,next)=>{
   .catch((err)=>{next({message:err})})
 })
 
+// Get a Director api/directos/:directorId
+const mongoose = require('mongoose')
+router.get('/:directorId',(req,res,next)=>{
+  DirectorModel.aggregate([
+    {
+      $match:{_id:mongoose.Types.ObjectId(req.params.directorId)}
+    }
+  ])
+  .then((data)=>{res.json(data)})
+  .catch((err)=>{next({message:err})})
+})
+
+// Create a new director in MongoDB
 router.post('/',(req, res, next)=>{
   const newDirector = new DirectorModel(req.body)
   newDirector.save()
